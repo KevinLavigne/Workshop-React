545eaa96a6cfa8313ba73e1225a87da4887214c0
KevinLavigne <lavigne.kevin02@gmail.com>
2022-05-02 14:33:57 +0200

    passage de props de Home.jsx to Hero.jsx pour construction du composant

---
+++ React-Beers-Shop/src/pages/Hero.jsx
@@ -0,0 +1,8 @@
+
+
+const Hero =(props) => {
+return(
+
+)
+}
+export default Hero
--- React-Beers-Shop/src/pages/Home.jsx
+++ React-Beers-Shop/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import react from "react";
-
+import home from "../data/home";
+import hero from "./Hero"
 const Home = () => {
   return (
+    <>
     <h1>Here is the home page</h1>
+    <Hero props={home.hero}/>
+    </>
   )
 }
 
